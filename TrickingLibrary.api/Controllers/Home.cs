434a99d93944bc3c937c6dba64c89194a6ca71ad
ahmed11dev <ahmed11dev@gmail.com>
2021-10-31 00:46:18 +0200

    init

---
+++ TrickingLibrary.api/Controllers/Home.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.api.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "hellow ahmed";
+        }
+    }
+}
